f30032a21f78d7889c0d50433dcfb278c48db77d
ilhan <ilhan.orhan007@gmail.com>
2016-03-01 18:45:06 +0200

    Solutions added

---
+++ SimpleArraySum/SimpleArraySum.js
@@ -0,0 +1,7 @@
+/*
+* You are given an array of integers of size N. 
+* Can you find the sum of the elements in the array?
+*/
+(function() {
+	return [1, 2, 3, 4, 10, 80].reduce(function(a, b){return a + b;});
+})();
---
+++ SolveMeFirst/SolveMeFirst.js
@@ -0,0 +1,9 @@
+/*
+* Can you create a function to return the sum of two integers,
+* passed as input parameters
+*/
+(function() {
+	function sumTwoInts(a, b) {
+		return a + b;
+	}
+})();
